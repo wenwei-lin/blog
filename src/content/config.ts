98ebd3ceab5a6fc86cd6ca39b8b2d705408e38cb
Wenwei Lin <wenwei66.lin@gmail.com>
2023-06-18 19:56:18 +0800

    build tag pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from "astro:content"
 
 export const collections = {
-    post: defineCollection({
+    posts: defineCollection({
         schema: z.object({
             title: z.string(),
             date: z.date(),
---
+++ src/content/posts/dotnet-learning-map.md
@@ -0,0 +1,9 @@
+---
+title: .NET学习地图
+slug: dotnet-learning-map
+date: 2023-06-18
+description: 本文介绍如何在Windows 10上安装WSL并配置开发环境。
+tags:
+    - .NET
+    - Windows
+---
